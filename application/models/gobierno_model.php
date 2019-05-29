0ee4629182bdd52730b88cc6122fadad6d4d2745
Facundo <reynafacu5@gmail.com>
2019-05-28 21:37:08 -0300

    config db and class goberment

---
+++ application/models/gobierno_model.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Gobierno_model extends CI_Model {
+
+	function __construct(){
+		parent::__construct();
+		$this->load->database();
+	}
+
+	public function nuevo_gobierno(){
+		
+	}
+	}
+?>
